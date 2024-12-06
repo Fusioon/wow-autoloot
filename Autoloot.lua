5dcee4790bf6a03e974084e87b4b817c62a93d00
Fusioon <Fusioon@users.noreply.github.com>
2024-12-06 06:22:16 +0100

    Fix loot window close timer, add forceLoot

--- Autoloot.lua
+++ Autoloot.lua
@@ -1,5 +1,15 @@
+-- Rarity comparison op
+local CMP_OP = {
+	EQ = 1,
+	NEQ = 2,
+	LT = 3,
+	LTE = 4,
+	GT = 5,
+	GTE = 6
+}
+
 local config = {
-	minRarity = 3, 	--  3 - rare 
+	minRarity = 2, 	--  3 - rare 
 	lootCoins = true,
 	lootCurrency = true,
 	lootQuestItems = true,
@@ -7,10 +17,15 @@ local config = {
 		enabled = true,
 		delay = 1500, -- delay in milliseconds,
 		disableKeys = { IsShiftKeyDown }
+	},
+	forceLoot = {
+		{ name = " Cloth", partial = true, rarity = 1 },
+		{ name = " Potion", partial = true, active = true }
 	}
 };
 
 local frame = CreateFrame("Frame", nil, UIParent);
+local currentTimer = nil;
 
 function CheckDisableKeys() 
 	for key,value in pairs(config.autoclose.disableKeys) do --actualcode
@@ -23,6 +38,45 @@ function CheckDisableKeys()
 end
 
 frame:RegisterEvent('LOOT_OPENED');
+
+function CompareValues(lhs, rhs, op)
+	if op == nil or op == CMP_OP_EQ then
+		return lhs == rhs;
+	end
+	if op == CMP_OP.NEQ then
+		return lhs ~= rhs;
+	end
+
+	if op == CMP_OP.LT then
+		return lhs < rhs;
+	end
+	if op == CMP_OP.LTE then
+		return lhs <= rhs;
+	end
+
+	if op == CMP_OP.GT then
+		return lhs > rhs;
+	end
+	if op == CMP_OP.GTE then
+		return lhs > rhs;
+	end
+
+	error("Unknown comparison operator")
+end
+
+function CheckForceLoot(name, rarity, quantity, active)
+	for i = 1, #config.forceLoot do
+		local fl = config.forceLoot[i];
+		if (fl.partial and string.find(name, fl.name)) or (not fl.partial and name == fl.name) then
+			if (fl.active == nil or fl.active == fl.active) and
+				(fl.rarity == nil or CompareValues(rarity, fl.rarity, fl.rarityCompareOP)) then
+					return true;
+			end
+		end
+	end
+
+	return false;
+end
 
 function LOOT_OPENED(autoloot)
 	
@@ -37,7 +91,8 @@ function LOOT_OPENED(autoloot)
 		local shouldLoot = (rarity >= config.minRarity) or
 						(config.lootCoins and lootType == 2) or 
 						(config.lootCurrency and lootType == 3) or 
-						(config.lootQuestItems and isQuestItem);
+						(config.lootQuestItems and isQuestItem) or 
+						CheckForceLoot(name, rarity, quantity, active);
 		
 		if shouldLoot and not locked then
 			LootSlot(i);
@@ -45,7 +100,10 @@ function LOOT_OPENED(autoloot)
 	end
 
 	if config.autoclose.enabled then
-		C_Timer.After(config.autoclose.delay / 1000, function() 
+		if currentTimer ~= nil then
+			currentTimer:Cancel();
+		end
+		currentTimer = C_Timer.NewTimer(config.autoclose.delay / 1000, function() 
 			if shouldClose and not CheckDisableKeys() then
 				CloseLoot();
 			end
